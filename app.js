e815255ca56a79a27c659cd206c3464718ba9db6
Kane Bernard <kaneon95@gmail.com>
2021-10-26 15:52:16 -0400

    Moved Css to the Public folder

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const rootDir = require('./util/path')
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
