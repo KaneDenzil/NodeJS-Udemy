ea0a3af6304380731b02b38b7ccc4466e43d8faa
Kane Bernard <kaneon95@gmail.com>
2021-10-27 13:23:27 -0400

    Added Handle Bars

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const express = require('express');
 const path = require('path');
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const rootDir = require('./util/path')
+
+const express = require('express');
+const expressHbs = require('express-handlebars');
 
 const app = express();
-app.set('view engine', 'pug');
+
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+);
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,7 +26,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
